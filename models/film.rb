9b5c3d45ec0480a63b60def6607c4982f8292954
Gregor Cox <gregorcox@gmail.com>
2018-06-08 12:18:25 +0100

    added a save method for film

--- models/film.rb
+++ models/film.rb
@@ -10,6 +10,13 @@ class Film
     @title = options['title']
     @price = options['price'].to_i
   end
-  
+
+  def save()
+    sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
+    values = [@title, @price]
+    film = SqlRunner.run(sql, values).first()
+    @id = film['id'].to_i
+  end
+
 
 end
